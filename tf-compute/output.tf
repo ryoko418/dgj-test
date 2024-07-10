9a8a145073ea289773b51b477e2ef262a282719e
ryoko.furuki <ryoko.furuki@ariseanalytics.com>
2024-07-10 15:14:20 +0900

    test1へ

--- tf-compute/output.tf
+++
@@ -1,4 +0,0 @@
-# The "name" of the availability domain to be used for the compute instance.
-output "name-of-first-availability-domain" {
-  value = data.oci_identity_availability_domains.ads.availability_domains[0].name
-}
